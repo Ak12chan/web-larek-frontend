68232d7a17b9742614117f9f235355b0acea371d
Ak12chan <134933334+Ak12chan@users.noreply.github.com>
2024-10-13 13:45:29 +0300

    Вторая половина WEB-ларёк. Исправление замечаний.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
 import './scss/styles.scss';
-import { EventEmitter } from "./components/base/events";
-import { AppApi } from "./components/AppApi";
-import { API_URL, CDN_URL } from "./utils/constants";
-import { ProductsData } from "./components/ProductsData";
-import { OrderData } from "./components/OrderData";
-import { Page } from "./components/Page";
-import { Modal } from "./components/common/Modal";
-import { Card } from "./components/Card";
-import { cloneTemplate } from "./utils/utils";
-import { IId, TOrderDelivery, TOrderContact, TOrderAnswer} from "./types";
-import { Basket } from "./components/Basket";
-import { Order } from "./components/Order";
-import { Contacts } from "./components/Contacts";
-import { Success } from "./components/Success";
+import { EventEmitter } from './components/base/events';
+import { AppApi } from './components/AppApi';
+import { API_URL, CDN_URL } from './utils/constants';
+import { ProductsData } from './components/ProductsData';
+import { OrderData } from './components/OrderData';
+import { Page } from './components/Page';
+import { Modal } from './components/common/Modal';
+import { Card } from './components/Card';
+import { cloneTemplate } from './utils/utils';
+import { IId, TOrderDelivery, TOrderContact, TOrderAnswer } from './types';
+import { Basket } from './components/Basket';
+import { Order } from './components/Order';
+import { Contacts } from './components/Contacts';
+import { Success } from './components/Success';
 
-const cardTemplate: HTMLTemplateElement = document.querySelector('#card-catalog');
-const cardPreviewTemplate: HTMLTemplateElement = document.querySelector('#card-preview');
-const cardBasketTemplate: HTMLTemplateElement = document.querySelector('#card-basket');
-const basketTemplate: HTMLTemplateElement = document.querySelector('#basket');
-const orderTemplate: HTMLTemplateElement = document.querySelector('#order');
-const contactsTemplate: HTMLTemplateElement = document.querySelector('#contacts');
-const successTemplate: HTMLTemplateElement = document.querySelector('#success');
-const modalContainer: HTMLElement = document.querySelector('#modal-container');
+// Приведение типов и проверка на null
+const cardTemplate = document.querySelector('#card-catalog') as HTMLTemplateElement | null;
+const cardPreviewTemplate = document.querySelector('#card-preview') as HTMLTemplateElement | null;
+const cardBasketTemplate = document.querySelector('#card-basket') as HTMLTemplateElement | null;
+const basketTemplate = document.querySelector('#basket') as HTMLTemplateElement | null;
+const orderTemplate = document.querySelector('#order') as HTMLTemplateElement | null;
+const contactsTemplate = document.querySelector('#contacts') as HTMLTemplateElement | null;
+const successTemplate = document.querySelector('#success') as HTMLTemplateElement | null;
+const modalContainer = document.querySelector('#modal-container') as HTMLElement | null;
+
+if (!cardTemplate || !cardPreviewTemplate || !cardBasketTemplate || !basketTemplate || !orderTemplate || !contactsTemplate || !successTemplate || !modalContainer) {
+	throw new Error('One or more required templates or containers are missing');
+}
+
 const events = new EventEmitter();
 const appApi = new AppApi(API_URL, CDN_URL);
 const productsData = new ProductsData(events);
@@ -29,63 +35,83 @@ const orderData = new OrderData(events);
 const page = new Page(document.body, events);
 const modal = new Modal(modalContainer, events);
 
-appApi.getProductList()
-	.then((data) => productsData.products = data)
-	.catch(err => console.error(err));
+// Создание статических экземпляров
+const basket = new Basket(cloneTemplate(basketTemplate), events);
+const order = new Order(cloneTemplate(orderTemplate), events);
+const contacts = new Contacts(cloneTemplate(contactsTemplate), events);
+const success = new Success(cloneTemplate(successTemplate), events);
+
+// Загрузка данных о продуктах
+appApi
+	.getProductList()
+	.then((data) => (productsData.products = data))
+	.catch((err) => console.error(err));
+
+// Обработчики событий
 events.on('products:changed', () => {
-	const cardArray = productsData.products.map(item => {
+	const cardArray = productsData.products.map((item) => {
 		const cardInstant = new Card(cloneTemplate(cardTemplate), events);
 		return cardInstant.render(item);
 	});
-	page.render({catalog: cardArray});
-})
-events.on ('card:select',  (item: IId) => {
+	page.render({ catalog: cardArray });
+});
+
+events.on('card:select', (item: IId) => {
 	const cardInstant = new Card(cloneTemplate(cardPreviewTemplate), events);
-	if (orderData.getProduct(item.id)) cardInstant.lockedButton(true)
-	modal.render({content: cardInstant.render(productsData.getProduct(item.id))});
-})
-events.on ('bids:add', (item: IId) => {
+	if (orderData.getProduct(item.id)) cardInstant.lockedButton(true);
+	modal.render({
+		content: cardInstant.render(productsData.getProduct(item.id)),
+	});
+});
+
+events.on('bids:add', (item: IId) => {
 	orderData.addProduct(productsData.getProduct(item.id));
+	events.emit('bids:change');
 	modal.close();
-})
-events.on ('bids:render', () => {
-	let count: number = 1;
-	const basket = new Basket(cloneTemplate(basketTemplate), events);
-	const basketArray = orderData.getProducts().map(item => {
+});
+
+events.on('bids:remove', (item: IId) => {
+	orderData.deleteProduct(item.id);
+	events.emit('bids:change');
+});
+
+events.on('bids:change', () => {
+	page.counter = orderData.getCount();
+});
+
+events.on('bids:render', () => {
+	let count = 1;
+	const basketArray = orderData.getProducts().map((item) => {
 		const cardInstant = new Card(cloneTemplate(cardBasketTemplate), events);
 		cardInstant.setIndex(count);
 		count++;
 		return cardInstant.render(item);
 	});
-	modal.render({content: basket.render({ list: basketArray , price: orderData.getTotal() }) });
-})
-events.on ('bids:remove', (item: IId) => {
-	orderData.deleteProduct(item.id);
-	events.emit('bids:render');
-})
-events.on('order:changed', () => {
-	page.counter = orderData.getCount();
+	modal.render({
+		content: basket.render({ list: basketArray, price: orderData.getTotal() }),
+	});
 });
-events.on ('order:render', () => {
-	const cardInstant = new Order(cloneTemplate(orderTemplate), events);
-	modal.render({content: cardInstant.render()});
-})
+
+events.on('order:render', () => {
+	modal.render({ content: order.render() });
+});
+
 events.on('contacts:render', (data: TOrderDelivery) => {
 	orderData.setDelivery(data);
-	const cardInstant = new Contacts(cloneTemplate(contactsTemplate), events);
-	modal.render({content: cardInstant.render()});
-})
+	modal.render({ content: contacts.render() });
+});
+
 events.on('success:render', (data: TOrderContact) => {
 	orderData.setContact(data);
 	console.log(orderData.getOrder());
 	appApi.postOrder(orderData.getOrder())
 		.then((data: TOrderAnswer) => {
 			orderData.clearOrder();
-			const cardInstant = new Success(cloneTemplate(successTemplate), events);
-			modal.render({content: cardInstant.render( {total: data.total} )});
+			modal.render({ content: success.render({ total: data.total }) });
 		})
-		.catch(err => console.error(err));
-})
+		.catch((err) => console.error(err));
+});
+
 events.on('success:close', () => modal.close());
 events.on('modal:open', () => {
 	page.locked(true);
@@ -93,3 +119,4 @@ events.on('modal:open', () => {
 events.on('modal:close', () => {
 	page.locked(false);
 });
+
